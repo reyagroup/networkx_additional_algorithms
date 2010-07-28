edee634d3d9a8c44bb667d752901fad24cc7cdca
Alex Levenson <alex@reyagroup.com>
2010-07-28 14:21:14 +0800

    Added unit test for ego net

--- unittests.py
+++ unittests.py
@@ -14,16 +14,29 @@ class TestSequenceFunctions(unittest.TestCase):
 		self.assertTrue(abs(x-y)<e)
 		
 	def setUp(self):
-		self.undirected = nx.read_edgelist("adp.edgelist",create_using=nx.Graph())
-		f = open("adp_constraints.pickle","r")
-		self.undirectedExpected = cPickle.load(f)
+		self.undirected = nx.read_edgelist("adp.edgelist",create_using=nx.Graph())		
 
-	def test_undirected_constraints(self):
+	def test_undirected_constraints_whole(self):
+		f = open("adp_constraints_whole.pickle","r")
+		expected = cPickle.load(f)
+		f.close()
+		
 		constraints = constraint.calcConstraints(self.undirected,False,True,True)
 		
 		for node in constraints:
 			for entry in constraints[node]:
-				self.assertWithin(constraints[node][entry],self.undirectedExpected[node][entry],0.000000001)
+				self.assertWithin(constraints[node][entry],expected[node][entry],0.000000001)
+				
+	def test_undirected_constraints_ego(self):
+		f = open("adp_constraints_ego.pickle","r")
+		expected = cPickle.load(f)
+		f.close()
+
+		constraints = constraint.calcConstraints(self.undirected,False,True,True)
+
+		for node in constraints:
+			for entry in constraints[node]:
+				self.assertWithin(constraints[node][entry],expected[node][entry],0.000000001)
 
 if __name__ == '__main__':
 	unittest.main()
