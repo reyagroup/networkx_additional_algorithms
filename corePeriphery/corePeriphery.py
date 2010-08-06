a1b50a7bc40c058f58b34b975ea98a2af52db943
Alex Levenson <alex@reyagroup.com>
2010-08-06 13:32:27 +0800

    seems to be working

--- corePeriphery/corePeriphery.py
+++ corePeriphery/corePeriphery.py
@@ -5,41 +5,63 @@
 # Thursday July 29th 2010
 
 import random
+import networkx as nx
+import numpy
 
-def _generateRandomPartition(A):
+def partition(graph):
+	A = nx.convert.to_numpy_matrix(graph)	
+	C = _kernighanLinOptimizer(A)
+	print  _simpleCorrelationToIdeal(A,C)
 	partition = {}
-	for x in xrange(len(A)):
-		partition[x] = random.randrange(2)
+	for node in graph.nodes():
+		partition[node] = C[graph.nodes().index(node)]
 	return partition
 	
-def _kernighanLinOptimizer(A,partition=None):
-	if not partition:
-		partition = _generateRandomPartition(A)
-	
-	locked = {}
-	for x in xrange(len(A)):
-		locked[x] = False
-	
-	currentScore = _simpleCorrelationToIdeal(A,partition)
-	
-	bestScore = _g(A,0,partition,currentScore)
-	bestX = 0
-	for x in xrange(1,len(A)):
-		if locked[x]: continue
-		g = _g(A,x,partition,currentScore)
-		print g
-		if g > bestScore: 
-			bestScore = g
-			bestX = x 
-	print "Best: " + str(bestScore)
+def _kernighanLinOptimizer(A,C=None,numIterations=3):
+	if not C:
+		# create a random partition of the graph into core / periphery
+		# 0 for periphery, 1 for core
+		C = numpy.random.randint(0,2,size=len(A))
+		# need to force one in each
+		print _simpleCorrelationToIdeal(A,C)
 
-def _g(A,a,partition,current):
-	partition[a] = (partition[a]+1)%2
-	new = _simpleCorrelationToIdeal(A,partition)
-	partition[a] = (partition[a]+1)%2
+	for t in xrange(numIterations):
+		print "on iteration: " + str(t)
+		CTentative = C.copy()
+		unlockedNodes = range(len(A))
+		gk = []
+		gkc = []
+		runningTotal = 0.0
+		while len(unlockedNodes) > 0:
+			currentScore = _simpleCorrelationToIdeal(A,CTentative)
+			bestScore = _g(A,unlockedNodes[0],CTentative,currentScore)
+			bestX = unlockedNodes[0]
+			for x in unlockedNodes:
+				g = _g(A,x,CTentative,currentScore)
+				if g > bestScore: 
+					bestScore = g
+					bestX = x
+			CTentative[bestX] = (CTentative[bestX]+1)%2
+			unlockedNodes.remove(bestX)
+			runningTotal += bestScore
+			gk.append(runningTotal)
+			gkc.append(CTentative.copy())
+	
+		bestScore = max(gk)
+		if bestScore > 0:
+			bestK = gk.index(max(gk))
+			C = gkc[bestK]
+		print  _simpleCorrelationToIdeal(A,C)
+	return C
+
+		
+def _g(A,a,C,current):
+	C[a] = (C[a]+1)%2
+	new = _simpleCorrelationToIdeal(A,C)
+	C[a] = (C[a]+1)%2
 	return new-current
 
-def _simpleCorrelationToIdeal(A,partition):
+def _simpleCorrelationToIdeal(A,C):
 	"""
 	Calculates how close to ideal A is in terms of
 	being a core / periphery structure
@@ -50,11 +72,9 @@ def _simpleCorrelationToIdeal(A,partition):
 	
 	# NOTE: should we include some idea of value/weighted?
 	
-	p = 0
-	size = len(A)
-	for i in xrange(size):
-		for j in xrange(size):
-			if A[i,j] > 0:
-				if partition[i] > 0 or partition[j] > 0:
-					p+=1
-	return p
+	a = numpy.repeat(numpy.matrix(C),len(C),axis=0)
+	b = numpy.repeat(numpy.matrix(C).transpose(),len(C),axis=1)
+	z = a + b
+	Cmask = numpy.divide(z,z)
+	
+	return = numpy.multiply(A,Cmask).sum()
