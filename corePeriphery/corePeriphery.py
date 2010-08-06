12354b189ba913d1c79a6df9b38944b4c1698221
Alex Levenson <alex@reyagroup.com>
2010-08-06 17:46:37 +0800

    renamed variables, wrote more comments

--- corePeriphery/corePeriphery.py
+++ corePeriphery/corePeriphery.py
@@ -6,7 +6,7 @@
 # simple discrete core periphery partitioner, based on the paper:
 # Computing Core/Periphery Structures and Permutation Tests for Social Relations Data
 #                                        by
-#               John P. Boyd†, William J. Fitzgerald, and Robert J. Beck
+#               John P. Boyd, William J. Fitzgerald, and Robert J. Beck
 #                  University of California at Irvine, CA 92697, USA
 
 
@@ -34,68 +34,96 @@ def partition(graph,partition=None,numIterations=3):
 	
 	if partition:
 		# convert the dictionary partition into a bit array
-		C = numpy.zeroes(len(A))
+		bitPartition = numpy.zeroes(len(A))
 		for node,value in partition.iteritems():
-			C[graph.nodes().index(node)] = int(value)
+			bitPartition[graph.nodes().index(node)] = int(value)
 	else:
 		# create a random partition of the graph into core / periphery
 		# 0 for periphery, 1 for core
-		C = numpy.random.randint(0,2,size=len(A))
-		C[0] = 0
-		C[1] = 1
+		bitPartition = numpy.random.randint(0,2,size=len(A))
+		# force at least one in each partition
+		bitPartition[0] = 0
+		bitPartition[1] = 1
 		
-	C = _kernighanLinOptimizer(A,C,numIterations)
+	bitPartition = _kernighanLinOptimizer(A,bitPartition,numIterations)
 	
 	# convert the numpy array to a dictionary
 	partition = {}
 	for node in graph.nodes():
-		partition[node] = C[graph.nodes().index(node)]
+		partition[node] = bitPartition[graph.nodes().index(node)]
 	return partition
 	
-def _kernighanLinOptimizer(A,C,numIterations):
+def _kernighanLinOptimizer(A,bitPartition,numIterations):
 	"""
 	Simple optimizer that tries to optimize similarity to the ideal
 	core / periphery matrix
 	"""
 	for t in xrange(numIterations):
-		CTentative = C.copy()
+		# need a place to store tentative swaps
+		tentativePartition = bitPartition.copy()
+		
+		# set all nodes to unlocked
 		unlockedNodes = range(len(A))
-		gk = []
-		gkc = []
-		runningTotal = 0.0
+		
+		# gains[i] holds the net gain for applying swaps 0 through i
+		gains = []
+		cumulativeGain = 0
+		
+		# tentativePartitions[i] holds the partion after swaps 0 through i have been applied
+		# TODO: quite memory inefficient... is there a better way to do this? (yes there is)
+		tentativePartitions = []
+		
+		# swap each node from it's current partition, in order of
+		# best net gain, keeping track of net gain as we go
 		while len(unlockedNodes) > 0:
-			currentScore = _simpleCorrelationToIdeal(A,CTentative)
-			bestScore = _g(A,unlockedNodes[0],CTentative,currentScore)
-			bestX = unlockedNodes[0]
-			for x in unlockedNodes:
-				g = _g(A,x,CTentative,currentScore)
-				if g > bestScore: 
-					bestScore = g
-					bestX = x
-			CTentative[bestX] = (CTentative[bestX]+1)%2
-			unlockedNodes.remove(bestX)
-			runningTotal += bestScore
-			gk.append(runningTotal)
-			gkc.append(CTentative.copy())
+			
+			# find the best node left to swap
+			currentScore = _simpleCorrelationToIdeal(A,tentativePartition)
+			bestGain = _gainDelta(A,unlockedNodes[0],tentativePartition,currentScore)
+			bestNode = unlockedNodes[0]			
+			for n in unlockedNodes:
+				gain = _gainDelta(A,n,tentativePartition,currentScore)
+				if gain > bestGain: 
+					bestGain = gain
+					bestNode = n
+			
+			# best node to swap found, perform the swap
+			tentativePartition[bestNode] = (tentativePartition[bestNode]+1)%2
+			unlockedNodes.remove(bestNode)
+			
+			# keep track of cumulativeGain thus far and save the current tentative
+			cumulativeGain += bestGain
+			gains.append(cumulativeGain)
+			tentativePartitions.append(tentativePartition.copy())
 	
-		bestScore = max(gk)
-		if bestScore > 0:
-			bestK = gk.index(max(gk))
-			C = gkc[bestK]
-	return C
+		# Now swaps 0...K...len(A) have been performed,
+		# now we need to find K, such that the sum of the gains
+		# from swaps 0...K is highest 
+		bestGain = max(gains)
+		
+		if bestGain > 0:
+			# Make the tentative swaps from 0 to K permanent swaps
+			bestK = gains.index(max(gains))
+			bitPartition = tentativePartitions[bestK]
+		else:
+			# if bestGain is negative then we're not going
+			# to find anything better using this algorithm
+			return bitPartition
+
+	return bitPartition
 
 		
-def _g(A,a,C,current):
+def _gainDelta(A,a,bitPartition,current):
 	"""
 	calculates the net gain achieved by swapping node a
 	from core to periphery or vice versa
 	"""
-	C[a] = (C[a]+1)%2
-	new = _simpleCorrelationToIdeal(A,C)
-	C[a] = (C[a]+1)%2
+	bitPartition[a] = (bitPartition[a]+1)%2
+	new = _simpleCorrelationToIdeal(A,bitPartition)
+	bitPartition[a] = (bitPartition[a]+1)%2
 	return new-current
 
-def _simpleCorrelationToIdeal(A,C):
+def _simpleCorrelationToIdeal(A,bitPartition):
 	"""
 	Calculates how close to ideal A is in terms of
 	being a core / periphery structure
@@ -107,9 +135,9 @@ def _simpleCorrelationToIdeal(A,C):
 	
 	# NOTE: should we include some idea of value/weighted?
 	
-	a = numpy.repeat(numpy.matrix(C),len(C),axis=0)
-	b = numpy.repeat(numpy.matrix(C).transpose(),len(C),axis=1)
+	a = numpy.repeat(numpy.matrix(bitPartition),len(bitPartition),axis=0)
+	b = numpy.repeat(numpy.matrix(bitPartition).transpose(),len(bitPartition),axis=1)
 	z = a + b
-	Cmask = numpy.divide(z,z)
+	bitPartitionmask = numpy.divide(z,z)
 	
-	return numpy.multiply(A,Cmask).sum()
+	return numpy.multiply(A,bitPartitionmask).sum()
