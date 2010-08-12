9bc0dae58e4ccd81dc4831179a3d42db69fff195
Alex Levenson <alex@reyagroup.com>
2010-08-12 14:25:39 +0800

    added in a third correlation function

--- corePeriphery/corePeriphery.py
+++ corePeriphery/corePeriphery.py
@@ -3,7 +3,7 @@
 # Reya Group
 # Thursday July 29th 2010
 
-# simple discrete core periphery partitioner, based on the paper:
+# simple discrete core peripher_correlationLessStricty partitioner, based on the paper:
 # Computing Core/Periphery Structures and Permutation Tests for Social Relations Data
 #                                        by
 #               John P. Boyd, William J. Fitzgerald, and Robert J. Beck
@@ -16,9 +16,10 @@ import numpy
 from geneticOptimizer import *
 
 class CorePeripheryOptimizer(GeneticOptimizer):
-	def __init__(self,A,populationSize,survivalRate,maxGenerations,mutateVsBreedRate):
-		GeneticOptimizer.__init__(self,populationSize,survivalRate,maxGenerations,mutateVsBreedRate)
+	def __init__(self,A,populationSize,survivalRate,maxGenerations,mutateVsBreedRate,quitAfterStable,correlationFunction):
+		GeneticOptimizer.__init__(self,populationSize,survivalRate,maxGenerations,mutateVsBreedRate,quitAfterStable)
 		self.A = A
+		self.correlation = correlationFunction
 		
 	def generateInitialPopulation(self):
 		return [numpy.random.randint(0,2,size=len(self.A)) for i in xrange(self.populationSize)]
@@ -39,66 +40,55 @@ class CorePeripheryOptimizer(GeneticOptimizer):
 		return child
 		
 	def getScore(self,dna):
-		#return _simpleCorrelationToIdeal(self.A,dna)
-		return _betterCorrelation(self.A,dna)
+		return self.correlation(self.A,dna)
 		
 	def statusReport(self,g,score):
 		print (g,score)
 
-def partition(graph,populationSize=100,survivalRate=0.75,maxGenerations=100,mutateVsBreedRate=0.5):
-	"""
-	Partitions graph into a core set and a periphery set
-	using a genetic optimizer
-	
-	graph: a networkx graph
-	populationSize, survivalRate,maxGenerations,mutateVsBreedRate: see geneticOptimizer.py
-	
-	returns: a dictionary mapping node->{0,1} (0 for periphery, 1 for core)
-	"""
-	
-	A = nx.convert.to_numpy_matrix(graph)
-	opt = CorePeripheryOptimizer(A,populationSize,survivalRate,maxGenerations,mutateVsBreedRate)
-	best = opt.optimize()
-
-	# convert the numpy array to a dictionary
-	partition = {}
-	for node in graph.nodes():
-		partition[node] = best[graph.nodes().index(node)]
-	return partition
-	
-def naiveOptimizer(graph,steps=10000):
-	"""
-	Partitions graph into a core set and a periphery set
-	using the worst optimizer (random explorer). All other methods should outperform this one.
-	
-	graph: a networkx graph
-	steps: how many solutions to explore
-	
-	returns: a dictionary mapping node->{0,1} (0 for periphery, 1 for core)
-	"""
-	A = nx.convert.to_numpy_matrix(graph)	
-
-	best = numpy.random.randint(0,2,size=len(A))
-	bestScore = _betterCorrelation(A,best)
-	for t in xrange(steps):
-		bitPartition = numpy.random.randint(0,2,size=len(A))
-		score = _betterCorrelation(A,bitPartition)
-		if score > bestScore:
-			bestScore = score
-			best = bitPartition.copy()
-
-	# convert the numpy array to a dictionary
-	partition = {}
-	for node in graph.nodes():
-		partition[node] = best[graph.nodes().index(node)]
-	return partition
-	
-def _betterCorrelation(A,bitPartition):
+def _correlationUseIsolateModel(A,bitPartition):
 	"""
 	Calculates how close to ideal A is in terms of
 	being a core / periphery structure
 	
 	A higher score is better
+	
+	Scored on:
+		Core-Core ties
+		Lack of Periph-Periph ties
+		Lack of Core-Periph / Periph-Core ties
+	
+	Not normalized, meaning bigger matrices will tend towards higher scores (I think)
+	"""	
+	a = numpy.repeat(numpy.matrix(bitPartition),len(bitPartition),axis=0)
+	b = numpy.repeat(numpy.matrix(bitPartition).transpose(),len(bitPartition),axis=1)
+	core_core_mask = numpy.bitwise_and(a,b)
+	periph_periph_mask = numpy.bitwise_and((a+1)%2,(b+1)%2)
+	involves_periph_mask = numpy.bitwise_or((a+1)%2,(b+1)%2)
+
+	diff = numpy.multiply(A,core_core_mask)
+	diff = diff - core_core_mask
+	diff = numpy.multiply(diff,diff)
+	lostPoints = diff.sum()	
+	
+	diff = numpy.multiply(A,periph_periph_mask)
+	lostPoints += diff.sum()
+
+	diff = numpy.multiply(A,involves_periph_mask)
+	lostPoints += diff.sum()
+
+	return len(a)**2 - lostPoints	
+	
+	
+def _correlationIgnoreCorePeriphTies(A,bitPartition):
+	"""
+	Calculates how close to ideal A is in terms of
+	being a core / periphery structure
+	
+	A higher score is better
+	
+	Scored on:
+		Core-Core ties
+		Lack of Periph-Periph ties
 	
 	Not normalized, meaning bigger matrices will tend towards higher scores (I think)
 	"""
@@ -118,12 +108,17 @@ def _betterCorrelation(A,bitPartition):
 
 	return len(a)**2 - lostPoints
 	
-def _simpleCorrelation(A,bitPartition):
+def _correlationIncludeCorePeriphTies(A,bitPartition):
 	"""
 	Calculates how close to ideal A is in terms of
 	being a core / periphery structure
 	
 	A higher score is better
+	
+	Scored on:
+		Core-Core ties
+		Lack of Periph-Periph ties
+		Core-Periph / Periph-Core ties
 	
 	Not normalized, meaning bigger matrices will tend towards higher scores (I think)
 	"""
@@ -132,4 +127,53 @@ def _simpleCorrelation(A,bitPartition):
 	involves_core_mask = numpy.bitwise_or(a,b)
 	diff = A - involves_core_mask
 	diff = numpy.multiply(diff,diff)
-	return len(A)**2 - (diff.sum())	
+	return len(A)**2 - (diff.sum())
+
+
+def partition(graph,populationSize=100,survivalRate=0.75,maxGenerations=100,mutateVsBreedRate=0.5,quitAfterStable=0.1,correlationFunction=_correlationIgnoreCorePeriphTies):
+	"""
+	Partitions graph into a core set and a periphery set
+	using a genetic optimizer
+	
+	graph: a networkx graph
+	populationSize, survivalRate,maxGenerations,mutateVsBreedRate: see geneticOptimizer.py
+	
+	returns: a dictionary mapping node->{0,1} (0 for periphery, 1 for core)
+	"""
+	
+	A = nx.convert.to_numpy_matrix(graph)
+	opt = CorePeripheryOptimizer(A,populationSize,survivalRate,maxGenerations,mutateVsBreedRate,quitAfterStable,correlationFunction)
+	best = opt.optimize()
+
+	# convert the numpy array to a dictionary
+	partition = {}
+	for node in graph.nodes():
+		partition[node] = best[graph.nodes().index(node)]
+	return partition
+	
+def naiveOptimizer(graph,steps=10000,correlationFunction=_correlationIgnoreCorePeriphTies):
+	"""
+	Partitions graph into a core set and a periphery set
+	using the worst optimizer (random explorer). All other methods should outperform this one.
+	
+	graph: a networkx graph
+	steps: how many solutions to explore
+	
+	returns: a dictionary mapping node->{0,1} (0 for periphery, 1 for core)
+	"""
+	A = nx.convert.to_numpy_matrix(graph)	
+
+	best = numpy.random.randint(0,2,size=len(A))
+	bestScore = correlationFunction(A,best)
+	for t in xrange(steps):
+		bitPartition = numpy.random.randint(0,2,size=len(A))
+		score = correlationFunction(A,bitPartition)
+		if score > bestScore:
+			bestScore = score
+			best = bitPartition.copy()
+
+	# convert the numpy array to a dictionary
+	partition = {}
+	for node in graph.nodes():
+		partition[node] = best[graph.nodes().index(node)]
+	return partition
