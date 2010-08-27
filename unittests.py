25b763094187ad8e2bebba30f57d42d6d0548964
Alex Levenson <alex@isnotinvain.com>
2010-08-27 16:50:27 +0800

    Added in unit test for triad census

--- unittests.py
+++ unittests.py
@@ -13,6 +13,7 @@ import agreement
 import constraint
 import coreness
 import brokerage
+import triadic_census
 import cPickle
 import itertools
 from scipy.stats.stats import pearsonr
@@ -20,6 +21,7 @@ from scipy.stats.stats import pearsonr
 class TestAdditionalAlgorithms(unittest.TestCase):
 		
 	def setUp(self):
+		self.random_graph = nx.read_edgelist("unit_test_data/random_graph.edgelist",create_using=nx.DiGraph())
 		self.directed = nx.read_edgelist("unit_test_data/bkoff_unweighted.edgelist",create_using=nx.DiGraph())
 		self.undirected = nx.Graph(self.directed)
 		self.partition = {"1":1,"2":1,"3":1,"4":1,"5":1,"6":1,"7":1,"8":1,"9":1,"10":1,"11":2,"12":2,"13":2,"14":2,"15":2,"16":2,"17":2,"18":2,"19":3,"20":3,"21":3,"22":3,"23":4,"24":4,"25":4,"26":4,"27":4,"28":4,"29":4,"30":4,"31":4,"32":4,"33":5,"34":5,"35":5,"36":6,"37":7,"38":8,"39":9,"40":9}
@@ -72,7 +74,13 @@ class TestAdditionalAlgorithms(unittest.TestCase):
 		r = pearsonr(cness,shouldBe)[0]
 		msg = "Correlation between calculated coreness and correct coreness is too low (" + str(r) + ")"
 		self.assert_(r>0.99,msg=msg)
-		
+	
+	def test_triadic_census(self):
+		cens = triadic_census.triadic_census(self.random_graph)
+		f = open("unit_test_data/random_triadic_census.pickle","r")
+		shouldBe = cPickle.load(f)
+		f.close()
+		self.assertEqual(cens,shouldBe)
 		
 if __name__ == '__main__':
 	unittest.main()
